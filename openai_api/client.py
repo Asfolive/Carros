39fbf04fe76e1859539ec802c794f34cde1fe666
Alex Oliveira <xxx.oliveira@gmail.com>
2024-09-27 16:17:46 -0300

    Update client.py

--- openai_api/client.py
+++ openai_api/client.py
@@ -5,7 +5,7 @@ def get_car_ai_bio(model, brand, year):
     Me mostre uma descrição de venda para o carro {} {} {} em apenas 250 caracteres. Fale coisas específicas desse modelo.
     '''.format(brand, model, year)
 
-    openai.api_key = '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'  # Substitua pela sua chave de API
+    openai.api_key = 'CHAVE_API'  # Substitua pela sua chave de API
 
     response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo',  # ou 'gpt-4', se preferir
